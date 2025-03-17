c755e0218e745a42d2c9b1f5b4641fbb93b318ce
アンNè <126953997+ThueCode24H@users.noreply.github.com>
2025-03-17 10:10:53 +0700

    update import ~
    17/03/2025

---
+++ src/components/cards/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <>
+      <div>
+        <h2>hello</h2>
+      </div>
+    </>
+  );
+}
---
+++ src/components/layout/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import card from '~/cards'
+
+export default function index() {
+  return (
+    <div>index</div>
+  )
+}
